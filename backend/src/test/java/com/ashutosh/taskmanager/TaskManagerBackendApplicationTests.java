492329d0aee6bb1d770b1aa0cbba1f40e6193e6b
Ashutosh Panwar <ashutoshpanwar07@gmail.com>
2026-09-22 12:24:30 +0530

    Make backend context test self contained

--- backend/src/test/java/com/ashutosh/taskmanager/TaskManagerBackendApplicationTests.java
+++ backend/src/test/java/com/ashutosh/taskmanager/TaskManagerBackendApplicationTests.java
@@ -3,11 +3,20 @@ package com.ashutosh.taskmanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:taskflow-test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.flyway.enabled=false",
+        "jwt.secret=01234567890123456789012345678901",
+        "jwt.expiration=3600000",
+        "jwt.refresh-expiration=604800000"
+})
 class TaskManagerBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
