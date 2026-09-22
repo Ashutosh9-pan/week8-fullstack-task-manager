75171ca987488e891c54217f643f4c012ea58c51
Ashutosh Panwar <ashutoshpanwar07@gmail.com>
2026-09-22 12:35:51 +0530

    Support collaborative task assignments

--- backend/src/main/java/com/ashutosh/taskmanager/repository/TaskRepository.java
+++ backend/src/main/java/com/ashutosh/taskmanager/repository/TaskRepository.java
@@ -1,10 +1,14 @@
 package com.ashutosh.taskmanager.repository;
+
 import com.ashutosh.taskmanager.entity.Task;
 import com.ashutosh.taskmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByUser(User user);
+    List<Task> findByUserOrAssigneeEmailIgnoreCase(User user, String assigneeEmail);
     Optional<Task> findByIdAndUser(Long id, User user);
+    Optional<Task> findByIdAndUserOrAssigneeEmailIgnoreCase(Long id, User user, String assigneeEmail);
 }
