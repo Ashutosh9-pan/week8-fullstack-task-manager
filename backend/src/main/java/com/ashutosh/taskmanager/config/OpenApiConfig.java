5171b302cf2676bb34e271b63219c52e3de1ae15
Ashutosh Panwar <ashutoshpanwar07@gmail.com>
2026-09-22 12:39:43 +0530

    Document TaskFlow API with OpenAPI security scheme

---
+++ backend/src/main/java/com/ashutosh/taskmanager/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.ashutosh.taskmanager.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "TaskFlow API",
+        version = "1.0",
+        description = "Secure task management REST API with JWT authentication"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer")
+public class OpenApiConfig {
+}
